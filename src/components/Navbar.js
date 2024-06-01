16840a8a3acd450ddff774f78a7944516fb476d7
nik379 <nikhilgupta3501@gmail.com>
2024-06-01 15:49:13 +0530

    changed Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,113 +1,73 @@
 import fullLogo from "../full_logo.png";
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router";
-import { ethers } from "ethers";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 
 function Navbar() {
-  const [connected, toggleConnect] = useState(false);
-  const location = useLocation();
-  const [currAddress, updateAddress] = useState("0x");
-  const [correctNetwork, setCorrectNetwork] = useState(false);
 
-  async function getAddress() {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const addr = await signer.getAddress();
-    updateAddress(addr);
-  }
+const [connected, toggleConnect] = useState(false);
+const location = useLocation();
+const [currAddress, updateAddress] = useState('0x');
 
-  function updateButton() {
-    const ethereumButton = document.querySelector(".enableEthereumButton");
-    ethereumButton.textContent = "Connected";
-    ethereumButton.classList.remove("hover:bg-[#2fb1b5]");
-    ethereumButton.classList.remove("bg-[#296f71]");
-    ethereumButton.classList.add("hover:bg-[#2fb1b5]");
-    ethereumButton.classList.add("bg-[#296f71]");
-  }
+async function getAddress() {
+  const ethers = require("ethers");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const addr = await signer.getAddress();
+  updateAddress(addr);
+}
 
-  // async function connectWebsite() {
-  //   const chainId = await window.ethereum.request({ method: "eth_chainId" });
-  //   if (chainId !== "0x5") {
-  //     //alert('Incorrect network! Switch your metamask network to Rinkeby');
-  //     await window.ethereum.request({
-  //       method: "wallet_switchEthereumChain",
-  //       params: [{ chainId: "0x2A" }],
-  //     });
-  //   }
-  //   await window.ethereum
-  //     .request({ method: "eth_requestAccounts" })
-  //     .then(() => {
-  //       updateButton();
-  //       console.log("here");
-  //       getAddress();
-  //       window.location.replace(location.pathname);
-  //     });
-  // }
+function updateButton() {
+  const ethereumButton = document.querySelector('.enableEthereumButton');
+  ethereumButton.textContent = "Connected";
+  ethereumButton.classList.remove("hover:bg-blue-70");
+  ethereumButton.classList.remove("bg-blue-500");
+  ethereumButton.classList.add("hover:bg-green-70");
+  ethereumButton.classList.add("bg-green-500");
+}
 
-  // useEffect(() => {
-  //   if (window.ethereum == undefined) return console.log("No ethereum");
-  //   let val = window.ethereum.isConnected();
-  //   if (val) {
-  //     console.log("here");
-  //     getAddress();
-  //     toggleConnect(val);
-  //     updateButton();
-  //   } else {
-  //     console.log("Not Connected!");
-  //   }
+async function connectWebsite() {
 
-  //   window.ethereum.on("accountsChanged", function (accounts) {
-  //     window.location.replace(location.pathname);
-  //   });
-  // });
-
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
-
-      if (!ethereum) {
-        console.log("Metamask not detected");
-        return;
-      }
-
-      let chainId = await ethereum.request?.({ method: "eth_chainId" });
-      console.log("Connected to chain:" + chainId);
-      const lineaSpeolia = "0x59141";
-
-      if (chainId !== lineaSpeolia) {
-        alert("You are not connected to the Linea Sepolia Testnet!");
-        updateAddress(".");
-        toggleConnect(false);
-        return;
-      } else {
-        setCorrectNetwork(true);
-        toggleConnect(true);
-      }
-
-
-      if(window.ethereum.isMetaMask) {
+    const chainId = await window.ethereum.request({ method: 'eth_chainId' });
+    if(chainId !== '0xe705')
+    {
+      //alert('Incorrect network! Switch your metamask network to Rinkeby');
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0xe705' }],
+     })
+    }  
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+      .then(() => {
         updateButton();
         console.log("here");
         getAddress();
-        toggleConnect(()=>true);
-        
-        window.location.replace(location.pathname);
-      } else {
-        console.log("MetaMask is not installed!");
-      }
-
-      const accounts = await ethereum.request?.({
-        method: "eth_requestAccounts",
+        window.location.replace(location.pathname)
       });
+}
 
-      console.log("Found account", accounts[0]);
-      updateAddress(accounts[0]);
-      toggleConnect(true);
-    } catch (error) {
-      console.log("Error connecting to metamask", error);
+  useEffect(() => {
+    let val = window.ethereum.isConnected();
+    if(val)
+    {
+      console.log("here");
+      getAddress();
+      toggleConnect(val);
+      updateButton();
     }
-  };
+
+    window.ethereum.on('accountsChanged', function(accounts){
+      window.location.replace(location.pathname)
+    })
+  });
+
 
   return (
     <div className="">
@@ -154,12 +114,12 @@ function Navbar() {
                 </li>
               )}
               <li>
-                {/* <button
+                <button
                   className="enableEthereumButton bg-[#296f71] hover:bg-[#2fb1b5] text-white font-bold py-2 px-4 rounded text-sm"
-                  onClick={connectWallet}
+                  onClick={connectWebsite}
                 >
                   {connected ? "Connected" : "Connect"}
-                </button> */}
+                </button>
               </li>
             </ul>
           </li>
